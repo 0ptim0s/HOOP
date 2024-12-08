e6f43a114a272f44b40eccdf1064993d62e3258e
0ptim0s <u23565536@tuks.co.za>
2024-12-08 13:53:55 +0200

    Did getMedia, SearchMedia, FilterMedia, UpdateMedia, DeleteMedia, AddMedia

--- api/api.php
+++ api/api.php
@@ -240,11 +240,11 @@
 
 
         public function getMedia(){
-            $randNum = 2;//rand(25,75);
-            $Query = "SELECT * FROM MEDIA";
+            $randNum = rand(25,75);
+            $Query = "SELECT * FROM MEDIA LIMIT ?";
             $SQLQuery = $this->db->prepare($Query);
             if($SQLQuery){
-                //$SQLQuery->bind_param();
+                $SQLQuery->bind_param("i",$randNum);
                 $didit = $SQLQuery->execute();
                 if($didit){
                     $res = $SQLQuery->get_result();
@@ -257,6 +257,121 @@
                 }else{$this->SQLEXECUTEEROR();}
             }else{$this->SQLERROR();}
         }//getAllMedia
+
+        public function SearchMedia($searchValue){
+            if(!isset($searchValue)){
+                $this->BadReq();
+            }
+
+            $Query = "SELECT * FROM MEDIA WHERE Title LIKE ? OR Genre LIKE ? OR Cast LIKE ?";
+            $SQLQuery = $this->db->prepare($Query);
+            if($SQLQuery){
+                $sv = "%" . $searchValue . "%";
+                $SQLQuery->bind_param("sss",$sv,$sv,$sv);
+                $didit = $SQLQuery->execute();
+                if($didit){
+                    $res = $SQLQuery->get_result();
+                    while($row = $res->fetch_assoc()){
+                        $media[] = $row;
+                    }
+                    $data = ["status"=> "200","data"=> $media];
+                    header("HTTP/1.1 200 OK");
+                    echo json_encode($data);
+                }else{$this->SQLEXECUTEEROR();}
+            }else{$this->SQLERROR();}
+        }//searchMedia
+
+        public function FilterMedia($genre){
+            if(!isset($genre)){
+                $this->BadReq();
+            }
+
+            $Query = "SELECT * FROM MEDIA WHERE Genre LIKE ?";
+            $SQLQuery = $this->db->prepare($Query);
+            if($SQLQuery){
+                $g = "%" . $genre . "%";
+                $SQLQuery->bind_param("s",$g);
+                $didit = $SQLQuery->execute();
+                if($didit){
+                    $res = $SQLQuery->get_result();
+                    while($row = $res->fetch_assoc()){
+                        $media[] = $row;
+                    }
+                    header("HTTP/1.1 200 OK");
+                    $data = ["status"=> "200","data"=> $media];
+                    echo json_encode($data);
+                }else{$this->SQLEXECUTEEROR();}
+            }else{$this->SQLERROR();}
+        }//FilterMedia
+
+
+        public function UpdateMedia($field, $value,$MEDIA_ID){
+            if(!isset($field) || !isset($value) || !isset($MEDIA_ID)){
+                $this->BadReq();
+            }
+
+            $Query = "UPDATE MEDIA SET " . $field . " = ? WHERE MEDIA_ID = ?";
+            $SQLQuery = $this->db->prepare($Query);
+            if($SQLQuery){
+                $SQLQuery->bind_param("si",$value, $MEDIA_ID);
+                $didit = $SQLQuery->execute();
+                if($didit){
+                    header("HTTP/1.1 200 OK");
+                    $data = ["status"=>"200","message"=>"Successfully updated ". $MEDIA_ID ."'s " . $field ." to ". $value];
+                    echo json_encode($data);
+                }else{$this->SQLEXECUTEEROR();}
+            }else{$this->SQLERROR();}
+        }//UpdateMedia (admin)
+
+        public function DeleteMedia($MEDIA_ID){
+            if(!isset($MEDIA_ID)){
+                $this->BadReq();
+            }
+
+            $Query = "DELETE FROM MEDIA WHERE MEDIA_ID = ? ";
+            $SQLQuery = $this->db->prepare($Query);
+            if($SQLQuery){
+                $SQLQuery->bind_param("i",$MEDIA_ID);
+                $didit = $SQLQuery->execute();
+                if($didit){
+                    header("HTTP/1.1 200 OK");
+                    $data = ["status"=>"200", "message"=>"Successfully deleted " . $MEDIA_ID];
+                    echo json_encode($data);
+                }else{$this->SQLEXECUTEEROR();}
+            }else{$this->SQLERROR();}
+        }//DeleteMedia (admin)
+
+        public function AddMedia($title,$desc,$genre,$age,$cast,$director,$producer,$poster,$relDate,$rating){
+            if(!isset($title) || !isset($desc) || !isset($genre) || !isset($cast) || !isset($director) || !isset($producer) || !isset($poster) || !isset($relDate)){
+                $this->BadReq();
+            }
+
+            if(!isset($age) && !isset($rating)){
+                $Query = "INSERT INTO MEDIA (Title, Description, Genre, Cast, Director, Producers, Poster, ReleaseDate) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+                $SQLQuery = $this->db->prepare($Query);
+                $SQLQuery->bind_param("ssssssss",$title,$desc,$genre,$cast,$director,$producer,$poster,$relDate);
+            }else if(!isset($rating)){
+                $Query = "INSERT INTO MEDIA (Title, Description, Genre, Age-Restriction, Cast, Director, Producers, Poster, ReleaseDate) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                $SQLQuery = $this->db->prepare($Query);
+                $SQLQuery->bind_param("sssisssss",$title,$desc,$genre,$age,$cast,$director,$producer,$poster,$relDate);
+            }else if(!isset($age)){
+                $Query = "INSERT INTO MEDIA (Title, Description, Genre, Cast, Director, Producers, Poster, ReleaseDate, Rating) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                $SQLQuery = $this->db->prepare($Query);
+                $SQLQuery->bind_param("ssssssssi",$title,$desc,$genre,$cast,$director,$producer,$poster,$relDate,$rating);
+            }
+
+            if($SQLQuery){
+                $didit = $SQLQuery->execute();
+                if($didit){
+                    header("HTTP/1.1 201 Created");
+                    $data = ["status"=>"201","message"=>"Successfully added ". $title];
+                    echo json_encode($data);
+                }else{$this->SQLEXECUTEEROR();}
+            }else{$this->SQLERROR();}
+        }//AddMedia (admin)
 
 
 
@@ -293,6 +408,26 @@
 
         if(isset($input_data) && $input_data['type'] == 'GetMedia'){
             $api->getMedia();
+        }
+
+        if(isset($input_data) && $input_data['type'] == 'SearchMedia'){
+            $api->SearchMedia($input_data['searchValue']);
+        }
+
+        if(isset($input_data) && $input_data['type'] == 'FilterMedia'){
+            $api->FilterMedia($input_data['genre']);
+        }
+
+        if(isset($input_data) && $input_data['type'] == 'UpdateMedia'){
+            $api->UpdateMedia($input_data['field'],$input_data['value'],$input_data['MEDIA_ID']);
+        }
+
+        if(isset($input_data) && $input_data['type'] == 'DeleteMedia'){
+            $api->DeleteMedia($input_data['MEDIA_ID']);
+        }
+
+        if(isset($input_data) && $input_data['type'] == "AddMedia"){
+            $api->AddMedia($input_data['title'],$input_data['Description'],$input_data['Genre'],$input_data['Age-Restriction'],$input_data['Cast'],$input_data['Director'],$input_data['Producer'],$input_data['Poster'],$input_data['ReleaseDate'],$input_data['rating']);
         }
 
     }//different endpoints for POST
